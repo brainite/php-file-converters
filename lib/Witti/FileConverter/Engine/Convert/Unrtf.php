93be1f345860235c5d92bfeda0a1a485ca38693e
Greg Payne <greg.payne@gmail.com>
2014-01-19 14:45:29 -0600

    Changed to getConvertFileShell.

--- lib/Witti/FileConverter/Engine/Convert/Unrtf.php
+++ lib/Witti/FileConverter/Engine/Convert/Unrtf.php
@@ -4,40 +4,22 @@ namespace Witti\FileConverter\Engine\Convert;
 use Witti\FileConverter\Engine\EngineBase;
 use Witti\FileConverter\Util\Shell;
 class Unrtf extends EngineBase {
-  public function convertFile($source, $destination) {
-    if (!isset($this->cmd)) {
-      return FALSE;
-    }
-
-    // Get the base converter object.
-    // Get a temporary file with the source extension since libre does not accept an output file name.
-    $s_path = $this->getTempFile($this->conversion[0]);
-    $d_path = str_replace('.' . $this->conversion[0], '.'
-      . $this->conversion[1], $s_path);
-    copy($source, $s_path);
-
-    // Convert the temporary file to the destination extension.
-    $output = $this->shell(array(
+  public function getConvertFileShell($source, &$destination) {
+    return array(
       $this->cmd,
       '--ps',
-      $s_path,
+      $source,
       Shell::arg('>', Shell::SHELL_SAFE),
-      $d_path,
-    ));
-    if (!is_file($d_path)) {
-      echo $output . "\n";
-    }
-
-    // Remove the original temporary file.
-    unlink($s_path);
-    // Move the converted temporary file to the destination.
-    rename($d_path, $destination);
+      $destination,
+    );
   }
 
   public function getHelpInstallation($os, $os_version) {
+    $output = "Unrtf is managed at http://www.gnu.org/software/unrtf/\n";
     switch ($os) {
       case 'Ubuntu':
-        return "sudo apt-get install unrtf";
+        $output .= "sudo apt-get install unrtf\n";
+        return $output;
     }
 
     return parent::getHelpInstallation();
--- lib/Witti/FileConverter/Engine/Optimize/JpegOptim.php
+++ lib/Witti/FileConverter/Engine/Optimize/JpegOptim.php
@@ -17,29 +17,13 @@ class JpegOptim extends EngineBase {
     ),
   );
 
-  public function convertFile($source, $destination) {
-    if (!isset($this->cmd)) {
-      return FALSE;
-    }
-
-    // Work with temporary files.
-    $ext = preg_replace('@~.*$@', '', $this->conversion[0]);
-    $s_path = $this->getTempFile($ext);
-    copy($source, $s_path);
-
-    // Convert the temporary file to the destination extension.
-    $shell = array();
-    $shell[] = $this->cmd;
-    $shell[] = Shell::argOptions($this->cmd_options, $this->configuration, NULL);
-    $shell[] = $s_path;
-    $output = $this->shell($shell);
-
-    // Move the converted temporary file to the destination.
-    if (!is_file($s_path)) {
-      echo $output . "\n";
-      return FALSE;
-    }
-    rename($s_path, $destination);
+  public function getConvertFileShell($source, &$destination) {
+    $destination = $source;
+    return array(
+      $this->cmd,
+      Shell::argOptions($this->cmd_options, $this->configuration, NULL),
+      $source,
+    );
   }
 
   public function getHelpInstallation($os, $os_version) {
