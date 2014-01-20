e4c10c16a4a5814e54dab8676a6735ab5ab61ccb
Greg Payne <greg.payne@gmail.com>
2014-01-19 21:28:51 -0600

    Changed to getConvertFileShell. Engine is still alpha.

--- lib/Witti/FileConverter/Engine/Convert/Ted.php
+++ lib/Witti/FileConverter/Engine/Convert/Ted.php
@@ -10,37 +10,13 @@ namespace Witti\FileConverter\Engine\Convert;
 
 use Witti\FileConverter\Engine\EngineBase;
 class Ted extends EngineBase {
-  private $scripts = array(
-    'rtf2pdf' => NULL,
-  );
-
-  public function convertFile($source, $destination) {
-    if (!isset($this->cmd)) {
-      return FALSE;
-    }
-
-    // Get the base converter object.
-    // Get a temporary file with the source extension since libre does not accept an output file name.
-    $s_path = $this->getTempFile($this->conversion[0]);
-    $d_path = str_replace('.' . $this->conversion[0], '.'
-      . $this->conversion[1], $s_path);
-    copy($source, $s_path);
-
-    // Convert the temporary file to the destination extension.
-    $output = $this->shell(array(
+  public function getConvertFileShell($source, &$destination) {
+    return array(
       $this->cmd,
       '--printToFile',
-      $s_path,
-      $d_path,
-    ));
-    if (!is_file($d_path)) {
-      echo $output . "\n";
-    }
-
-    // Remove the original temporary file.
-    unlink($s_path);
-    // Move the converted temporary file to the destination.
-    rename($d_path, $destination);
+      $source,
+      $destination,
+    );
   }
 
   public function getHelpInstallation($os, $os_version) {
