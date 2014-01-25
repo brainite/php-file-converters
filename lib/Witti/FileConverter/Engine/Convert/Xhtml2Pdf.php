ec9818f970ec16228597d19880b331a53ae7a0d6
Greg Payne <greg.payne@gmail.com>
2014-01-25 12:54:53 -0600

    Implemented the xhtml2pdf engine.

--- lib/Witti/FileConverter/Engine/Convert/Xhtml2Pdf.php
+++ lib/Witti/FileConverter/Engine/Convert/Xhtml2Pdf.php
@@ -4,77 +4,57 @@ namespace Witti\FileConverter\Engine\Convert;
 use Witti\FileConverter\Engine\EngineBase;
 use Witti\FileConverter\Util\Shell;
 class Xhtml2Pdf extends EngineBase {
-  // Set default properties
-//   $this->set('html', 1)
-//   ->set('base', 'http://' . $_SERVER['HTTP_HOST'] . '/');
-//     // Build the make options.
-//     $validOpts = Array(
-//         'base',
-//         'css',
-//         'encoding',
-//         'xml',
-//         'html',
-//     );
-//     $optStr = '';
-//     foreach ($this->getByArray($validOpts) as $k => $v) {
-//       if ($v) {
-//         $optStr .= " --$k ";
-//         if ($v != 1) {
-//           $optStr .= $v . ' ';
-//         }
-//       }
-//     }
+  protected $cmd_options = array(
+    array(
+      'name' => 'quiet',
+      'mode' => Shell::SHELL_ARG_BOOL_DBL,
+      'default' => TRUE,
+    ),
+    array(
+      'name' => 'css-dump',
+      'mode' => Shell::SHELL_ARG_BOOL_DBL,
+    ),
+    array(
+      'name' => 'debug',
+      'mode' => Shell::SHELL_ARG_BOOL_DBL,
+    ),
+    array(
+      'name' => 'warn',
+      'mode' => Shell::SHELL_ARG_BOOL_DBL,
+    ),
+    array(
+      'name' => 'xhtml',
+      'description' => 'Same as --xml, opposite of --html (implied arg)',
+      'mode' => Shell::SHELL_ARG_BOOL_DBL,
+      'default' => FALSE,
+    ),
+    array(
+      'name' => 'base',
+      'description' => 'Base path',
+      'mode' => Shell::SHELL_ARG_BASIC_DBL,
+    ),
+    array(
+      'name' => 'css',
+      'description' => 'CSS File',
+      'mode' => Shell::SHELL_ARG_BASIC_DBL,
+    ),
+    array(
+      'name' => 'encoding',
+      'description' => 'Character encoding',
+      'mode' => Shell::SHELL_ARG_BASIC_DBL,
+    ),
+  );
 
-//     // Convert the HTML using pisa
-//     $cmd = "cat '$old' | xhtml2pdf -q $optStr '-' > '$new'";
-//     $errMsg = exec($cmd);
-
-//     // Pisa seems to create an empty -.pdf file when '-' is specified for stdout.
-//     if (is_file('-.pdf')) {
-//       @unlink('-.pdf');
-//     }
-
-  public function convertFile($source, $destination) {
-    if (!isset($this->cmd)) {
-      return FALSE;
-    }
-
-    $conf = array_merge(array(
-      'quiet' => FALSE,
-    ), $this->configuration);
-
-    // Work with temporary files.
-    $s_path = $this->getTempFile($this->conversion[0]);
-    $d_path = str_replace('.' . $this->conversion[0], '.'
-      . $this->conversion[1], $s_path);
-    copy($source, $s_path);
-
-    // Convert the temporary file to the destination extension.
-    $shell = array();
-    $shell[] = $this->cmd;
-    $shell[] = Shell::argDouble('quiet', $conf['quiet']);
-    $shell[] = $s_path;
-    $shell[] = $d_path;
-    $output = $this->shell($shell);
-
-    // Remove the original temporary file.
-    unlink($s_path);
-
-    // Move the converted temporary file to the destination.
-    if (!is_file($d_path)) {
-      echo $output . "\n";
-      return FALSE;
-    }
-    rename($d_path, $destination);
-
-    //     xhtml2pdf [--base=base path] [--css=CSS file] [--css-dump] [--debug]
-    //     [--encoding=character encoding] [--quiet] [--warn] [--xml]
-    //     [--xhtml] [--html] [SRC] [DEST]
+  public function getConvertFileShell($source, &$destination) {
+    return array(
+      $this->cmd,
+      Shell::argOptions($this->cmd_options, $this->configuration),
+      $source,
+      $destination,
+    );
   }
 
   public function getHelpInstallation($os, $os_version) {
-    return "This engine is a placeholder and is not yet ready for use.";
-
     $output = "Xhtml2Pdf can be found online at http://www.xhtml2pdf.com/\n";
     switch ($os) {
       case 'Ubuntu':
@@ -86,7 +66,18 @@ class Xhtml2Pdf extends EngineBase {
     return parent::getHelpInstallation();
   }
 
+  public function getVersionInfo() {
+    $info = array(
+      'xhtml2pdf' => $this->shell($this->cmd . " --version")
+    );
+    if (preg_match('@pisa ([\d\.]+)@', $info['xhtml2pdf'], $arr)) {
+      $info['xhtml2pdf'] = $arr[1];
+    }
+    return $info;
+  }
+
   public function isAvailable() {
-    return FALSE;
+    $this->cmd = $this->shellWhich('xhtml2pdf');
+    return isset($this->cmd);
   }
 }
